552e308ba7e0c9c7c45a42e23ba2b89363b221c3
VeeK9 <vytautas.kakaras@gmail.com>
2024-02-23 11:31:46 +0200

    Hero section

--- React/46 paskaita - 02.23 - React Ivadas/pirmasis/src/components/Hero.jsx
+++ React/46 paskaita - 02.23 - React Ivadas/pirmasis/src/components/Hero.jsx
@@ -1,8 +1,9 @@
 const Hero = () => {
   return (  
-    <>
-      <p>Hero</p>
-    </>
+    <section id="hero">
+      <h1>React <br/> is <br /> POG</h1>
+      <img src="https://blog.logrocket.com/wp-content/uploads/2023/11/react-server-components-comprehensive-guide.png" alt="El Capital" />
+    </section>
   );
 }
  
