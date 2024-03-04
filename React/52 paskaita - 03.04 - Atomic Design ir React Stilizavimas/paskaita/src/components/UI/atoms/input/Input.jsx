71c9e271fcbe5f8d0da1eb837d52dcbecf826fc6
VeeK9 <vytautas.kakaras@gmail.com>
2024-03-04 13:34:06 +0200

    input styled-components

--- React/52 paskaita - 03.04 - Atomic Design ir React Stilizavimas/paskaita/src/components/UI/atoms/input/Input.jsx
+++ React/52 paskaita - 03.04 - Atomic Design ir React Stilizavimas/paskaita/src/components/UI/atoms/input/Input.jsx
@@ -1,9 +1,21 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  background-color: orange;
+  color: black;
+  font-size: 12px;
+  letter-spacing: 2px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: 1px solid black;
+`;
+
 const Input = ({type, name, id, placeholder, valueState, onChangeF}) => {
 
-  // console.log(valueState)
+  // console.log(valueState);
 
   return (
-    <input
+    <StyledInput
       type={type}
       name={name}
       id={id}
