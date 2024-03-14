1ce8b6782aff0886c769ff94604d5e2a96650720
VeeK9 <vytautas.kakaras@gmail.com>
2024-03-14 15:19:40 +0200

    paskaita baigta

--- React/59 paskaita - 03.14 - Formik ir Yup/src/components/pages/Register.jsx
+++ React/59 paskaita - 03.14 - Formik ir Yup/src/components/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useFormik } from "formik";
-import styled from "styled-components"
+import styled from "styled-components";
+import * as Yup from "yup";
 
 const StyledSection = styled.section`
   display: flex;
@@ -8,7 +9,6 @@ const StyledSection = styled.section`
   > form {
     display: flex;
     flex-direction: column;
-    width: 30vw;
     gap: 20px;
     > input {
       width: 30%;
@@ -19,11 +19,17 @@ const StyledSection = styled.section`
     }
     > div {
       display: grid;
+      gap: 20px;
       grid-template-columns: 1fr 1fr;
       justify-content: space-between;
       > div {
         display: flex;
         justify-content: space-evenly;
+      }
+      > span {
+        grid-column-start: span 2;
+        text-align: center;
+        color: red;
       }
     }
   }
@@ -44,7 +50,45 @@ const Register = () => {
     },
     onSubmit: values => {
       console.log(values);
-    }
+      const newUser= {
+        ...values,
+        avatarURL: values.avatarURL ? values.avatarURL : 'https://static.vecteezy.com/system/resources/thumbnails/020/911/740/small/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png'
+      }
+      console.log(newUser)
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(3, 'Username must be at least 3 symbols.')
+        .max(25, 'Username must be less than 25 symbols')
+        .required('Username is required')
+        .trim(),
+      email: Yup.string()
+        .email('Enter a valid email!')
+        .required('Email is required')
+        .trim(),
+      avatarURL: Yup.string()
+        .url('Must be a valid URL')
+        .default('https://static.vecteezy.com/system/resources/thumbnails/020/911/740/small/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png')
+        .trim(),
+      age: Yup.number()
+        .integer('Must be a number without commas')
+        .min(18, 'Must be at least 18yo')
+        .required('Your age is required'),
+      gender: Yup.string()
+        .required('Must choose one'),
+      language: Yup.string()
+        .required('Must select a language'),
+      password: Yup.string()
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,25}$/,
+          'Password must be at least least 8 symbols but not exceed 25 symbols'
+        )
+        .required('Required')
+        .trim(),
+      passwordConfirm: Yup.string()
+        .oneOf([Yup.ref('password')], "Passwords don't match.")
+        .required('Required')
+    })
   });
 
   return (
@@ -58,8 +102,10 @@ const Register = () => {
             name="username" id="username"
             placeholder="Create your username"
             value={formik.values.username}
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
           />
+          {formik.touched.username && formik.errors.username && <span>{formik.errors.username}</span>}
         </div>
         <div>
           <label htmlFor="email">Email:</label>
@@ -69,7 +115,9 @@ const Register = () => {
             placeholder="Enter You email"
             value={formik.values.email}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.email && formik.errors.email && <span>{formik.errors.email}</span>}
         </div>
         <div>
           <label htmlFor="avatarURL">Avatar:</label>
@@ -79,7 +127,9 @@ const Register = () => {
             placeholder="Paste the URL of your profile picture"
             value={formik.values.avatarURL}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.avatarURL && formik.errors.avatarURL && <span>{formik.errors.avatarURL}</span>}
         </div>
         <div>
           <label htmlFor="age">Age:</label>
@@ -87,9 +137,12 @@ const Register = () => {
             type="number"
             name="age" id="age"
             placeholder="Enter your age"
+            step={1}
             value={formik.values.age}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.age && formik.errors.age && <span>{formik.errors.age}</span>}
         </div>
         <div>
           <label htmlFor="">Select your gender:</label>
@@ -109,6 +162,7 @@ const Register = () => {
               onChange={formik.handleChange}
             />
           </div>
+          {formik.touched.gender && formik.errors.gender && <span>{formik.errors.gender}</span>}
         </div>
         <div>
           <label htmlFor="language">Select your native language:</label>
@@ -116,6 +170,7 @@ const Register = () => {
             id="language"
             value={formik.values.language}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           >
             <option value="" disabled></option>
             <option value="lithuanian">Lithuanian</option>
@@ -123,6 +178,7 @@ const Register = () => {
             <option value="spanish">Spanish</option>
             <option value="polish">Polish</option>
           </select>
+          {formik.touched.language && formik.errors.language && <span>{formik.errors.language}</span>}
         </div>
         <div>
           <label htmlFor="password">Password:</label>
@@ -132,7 +188,9 @@ const Register = () => {
             placeholder="********"
             value={formik.values.password}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.password && formik.errors.password && <span>{formik.errors.password}</span>}
         </div>
         <div>
           <label htmlFor="passwordConfirm">Confirm Password:</label>
@@ -142,7 +200,9 @@ const Register = () => {
             placeholder="********"
             value={formik.values.passwordConfirm}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.passwordConfirm && formik.errors.passwordConfirm && <span>{formik.errors.passwordConfirm}</span>}
         </div>
           <input type="submit" value="Register"/>
       </form>
